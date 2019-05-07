9212ac27b2389ef70e125f4f14dc64ec487d8128
Kamil <kamil@czanik.xyz>
2019-05-07 16:19:58 +0200

    Contract update

--- app/src/main/java/xyz/czanik/dafttap/tap_game/TapGameMVP.kt
+++ app/src/main/java/xyz/czanik/dafttap/tap_game/TapGameMVP.kt
@@ -1,19 +1,30 @@
 package xyz.czanik.dafttap.tap_game
 
-import android.app.AlertDialog
 import xyz.czanik.dafttap.TapRecord
 
 interface TapGameMVP {
 
     interface View {
-        fun display(alertDialog: AlertDialog)
+        val presenter: Presenter
+        var isTapViewEnabled: Boolean
+
+        fun displayDialogWith(message: String,title: String)
+        fun showMessage(message: String)
+        fun updateScore(score: Int)
+        fun updateTime(time: Long)
     }
 
     interface Presenter {
+        val view: View
+        val model: Model
+
         fun onTap()
+        fun prepareGame()
     }
 
     interface Model {
+        var time: Long
+        var taps: Int
         val ranking : ArrayList<TapRecord>
 
         fun saveRanking()
